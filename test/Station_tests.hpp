1f9263dbdf4b72f7c6ab6208a212822895d3fbd8
larsfinlay <larsfinlay@googlemail.com>
2019-08-14 07:46:57 +0100

    Refactored Test Code

--- test/Station_tests.hpp
+++ test/Station_tests.hpp
@@ -4,9 +4,38 @@
 
 using namespace std;
 
+// Overload station builds for name set and not
+Station* BuildStation();
+Station* BuildStation(string name);
+
+// Deallocate heap pointer
+void DeallocateStationPtr(Station* stationPtr);
+
 TEST_CASE( "Station is instantiated with passed name", "[Station]" )
 {
   string expectedName("Waterloo");
-  Station station(expectedName);
-  REQUIRE( station.GetName() == expectedName );
+  Station* stationPtr = BuildStation(expectedName);
+  REQUIRE( stationPtr->GetName() == expectedName );
+  DeallocateStationPtr(stationPtr);
+}
+
+// Using name "default"
+Station* BuildStation()
+{
+  Station* stationPtr = new Station("default");
+  return stationPtr;
+}
+
+// Using passed name
+Station* BuildStation(string name)
+{
+  Station* stationPtr = new Station(name);
+  return stationPtr;
+}
+
+// Deallocate Station pointer
+void DeallocateStationPtr(Station* stationPtr)
+{
+  delete stationPtr;
+  stationPtr = nullptr;
 }
--- test/Train_tests.hpp
+++ test/Train_tests.hpp
@@ -4,16 +4,45 @@
 
 using namespace std;
 
+// Allocate heap memory to local Train Object
+Train* BuildTrain();
+Train* BuildTrain(string name);
+
+// Deallocate heap memory to avoid memory leaks
+void DeallocateTrainPtr(Train* tPtr);
+
 TEST_CASE( "Train is instantiated with passed name", "[Train]" )
 {
-  string expectedName("SWR044");
-  Train train(expectedName);
-  REQUIRE( train.GetName() == expectedName );
+  string expectedName("SWR042");
+  Train* trainPtr = BuildTrain(expectedName);
+  REQUIRE( trainPtr->GetName() == expectedName );
+  DeallocateTrainPtr(trainPtr);
 }
 
 TEST_CASE( "Train has moving bool which is initialised as false", "[Train]" )
 {
-  bool expectedMotionState(false);
-  Train train("Filler for now, should be instantiated at top!");
-  REQUIRE( train.IsMoving() == expectedMotionState ); // This can surely be improved, like an auto false asserter
+  Train* trainPtr = BuildTrain();
+  REQUIRE_FALSE( trainPtr->IsMoving() );
+  DeallocateTrainPtr(trainPtr);
+}
+
+// Allocate heap memory to local Train Object
+Train* BuildTrain()
+{
+  Train* trainPtr = new Train("default");
+  return trainPtr;
+}
+
+// Allocate heap memory to local Train Object with passed string for name
+Train* BuildTrain(string name)
+{
+  Train* trainPtr = new Train(name);
+  return trainPtr;
+}
+
+// Deallocate heap memory
+void DeallocateTrainPtr(Train* tPtr)
+{
+  delete tPtr;
+  tPtr = nullptr;
 }
